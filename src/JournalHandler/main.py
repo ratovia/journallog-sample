2c1ce6414f280bc9d844a76803406b8cb49816b5
ratovia <ratovia@hotmail.co.jp>
2023-07-11 12:03:39 +0900

    修正

--- src/JournalHandler/main.py
+++ src/JournalHandler/main.py
@@ -2,24 +2,24 @@ import logging
 import logging.config
 from systemd import journal
 
-logging_config = dict(
-    version=1,
-    formatters={
-        'f': {'format':
-              'APP-LOG-LEVEL:%(levelname)-8s %(message)s'}
-    },
-    handlers={
-        'h': {'class': 'logging.StreamHandler',
-              'formatter': 'f',
-              'level': logging.DEBUG}
-    },
-    root={
-        'handlers': ['h'],
-        'level': logging.DEBUG,
-    },
-)
+# logging_config = dict(
+#     version=1,
+#     formatters={
+#         'f': {'format':
+#               'APP-LOG-LEVEL:%(levelname)-8s %(message)s'}
+#     },
+#     handlers={
+#         'h': {'class': 'logging.StreamHandler',
+#               'formatter': 'f',
+#               'level': logging.DEBUG}
+#     },
+#     root={
+#         'handlers': ['h'],
+#         'level': logging.DEBUG,
+#     },
+# )
 
-logging.config.dictConfig(logging_config)
+# logging.config.dictConfig(logging_config)
 
 logger = logging.getLogger()
 logger.addHandler(journal.JournalHandler())
