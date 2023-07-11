2aa6259e46d329b15046afc2c54cf2aa9f05a62e
ratovia <ratovia@hotmail.co.jp>
2023-07-11 13:00:24 +0900

    やってみる

--- src/JournalHandler/main.py
+++ src/JournalHandler/main.py
@@ -1,37 +1,18 @@
 import logging
-import logging.config
 from systemd.journal import JournalHandler
 
-# ログ設定辞書
-logging_config = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "standard": {
-            "format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s"
-        }
-    },
-    "handlers": {
-        "journal": {
-            "class": "systemd.journal.JournalHandler",
-            "level": "DEBUG",
-            "formatter": "standard"
-        }
-    },
-    "loggers": {
-        "": {
-            "handlers": ["journal"],
-            "level": "DEBUG",
-            "propagate": True
-        }
-    }
-}
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
-# ログ設定の適用
-logging.config.dictConfig(logging_config)
+journal_handler = JournalHandler()
+journal_handler.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(name)s: %(message)s')
+journal_handler.setFormatter(formatter)
+
+logger.addHandler(journal_handler)
 
 # ログメッセージの記録
-logger = logging.getLogger(__name__)
 logger.debug('This is a debug message')
 logger.info('This is an info message')
 logger.warning('This is a warning message')
