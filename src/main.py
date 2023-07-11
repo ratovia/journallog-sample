099cbf02589401384f9259b5c77fa4979d51cd6b
ratovia <ratovia@hotmail.co.jp>
2023-07-11 10:42:48 +0900

    formatを修正

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ logging_config = dict(
     version=1,
     formatters={
         'f': {'format':
-              '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'}
+              '%(asctime)s %(levelname)-8s JOURNAL-LOG-SAMPLE %(message)s'}
     },
     handlers={
         'h': {'class': 'logging.StreamHandler',
