946a1e17667b7f6c021cde211fbe4148c33cc6ec
ratovia <ratovia@hotmail.co.jp>
2023-07-11 13:05:32 +0900

    試してみる

--- src/JournalHandler/main.py
+++ src/JournalHandler/main.py
@@ -1,18 +1,9 @@
 import logging
-from systemd.journal import JournalHandler
+from systemd import journal
+logger = logging.getLogger('custom_logger_name')
+logger.addHandler(journal.JournalHandler(SYSLOG_IDENTIFIER='custom_unit_name'))
+logger.warning("Some message: %s", 'detail')
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-
-journal_handler = JournalHandler()
-journal_handler.setLevel(logging.DEBUG)
-
-formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(name)s: %(message)s')
-journal_handler.setFormatter(formatter)
-
-logger.addHandler(journal_handler)
-
-# ログメッセージの記録
 logger.debug('This is a debug message')
 logger.info('This is an info message')
 logger.warning('This is a warning message')
