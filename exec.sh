183c8484991910d135a287a272123c9995e87b98
ratovia <ratovia@hotmail.co.jp>
2023-07-11 11:07:01 +0900

    修正

--- exec.sh
+++ exec.sh
@@ -1,2 +1,8 @@
 # StreamHandler
-python3 src/StreamHandler/main.py 2>&1 | logger -t "JournalSample"
+python3 src/StreamHandler/main.py 2>&1 | logger -t "Journal-StreamHandler"
+
+# journal-lib
+python3 src/journal-lib/main.py 2>&1 | logger -t "Journal-journal-lib"
+
+# JournalHandler
+python3 src/JournalHandler/main.py 2>&1 | logger -t "Journal-JournalHandler"
